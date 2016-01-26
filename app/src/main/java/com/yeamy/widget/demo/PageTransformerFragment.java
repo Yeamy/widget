850f6e8510f48da057ec5bac9f8c608a2d11f61a
Yeamy <Yeamy0754@Yeamys-MacBook-Pro.local>
2016-01-26 17:36:48 +0800

    fix bug

--- app/src/main/java/com/yeamy/widget/demo/PageTransformerFragment.java
+++ app/src/main/java/com/yeamy/widget/demo/PageTransformerFragment.java
@@ -122,7 +122,7 @@ public class PageTransformerFragment extends Fragment {
                 TextView textView = (TextView) rootView.findViewById(R.id.section_label);
                 int position = getArguments().getInt(ARG_SECTION_NUMBER);
                 textView.setText("Hello World from section: " + (position + 1));
-                int[] colors = {0xfff9bdbb, 0xff80cbc4, 0xffb3e6fc};
+                int[] colors = {0xfff9bdbb, 0xff80cbc4, 0xffb3e6fc, 0xfff9bdbb, 0xff80cbc4};
                 rootView.setBackgroundColor(colors[position]);
                 return rootView;
             }
