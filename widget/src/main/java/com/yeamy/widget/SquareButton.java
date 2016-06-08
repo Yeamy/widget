781959e4a8942426bb0bbd670fe9b515ee3ca236
Yeamy <Yeamy0754@Yeamys-MacBook-Pro.local>
2016-06-08 16:38:22 +0800

    square button

---
+++ widget/src/main/java/com/yeamy/widget/SquareButton.java
@@ -0,0 +1,57 @@
+package com.yeamy.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.Button;
+
+/**
+ * A square button with center Gravity
+ */
+public class SquareButton extends Button {
+
+    public SquareButton(Context context) {
+        super(context);
+        setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+    }
+
+    public SquareButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+    }
+
+    public SquareButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+    }
+
+    @SuppressWarnings("NewApi")
+    public SquareButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(MeasureSpec.getSize(widthMeasureSpec),
+                MeasureSpec.getMode(heightMeasureSpec));
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        if (changed) {
+            Drawable drawable = getCompoundDrawables()[1];
+            if (drawable != null) {
+                int h = drawable.getBounds().height()
+                        + getCompoundDrawablePadding()
+                        + (int) getTextSize();
+                int padding = (getHeight() - h) / 2;
+                setPadding(0, padding, 0, 0);
+            }
+        }
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+}
